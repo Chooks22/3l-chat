c24a7af96049be488e97d4c2209a149174cc8521
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2022-09-27 18:11:27 +0800

    🐛 FIX: patch swiper types

--- src/swiper.d.ts
+++ src/swiper.d.ts
@@ -2,5 +2,7 @@ declare namespace svelte.JSX {
   // eslint-disable-next-line @typescript-eslint/consistent-indexed-object-style, @typescript-eslint/no-unused-vars
   interface HTMLAttributes<T> {
     [key: `data-${string}`]: string
+    modules?: unknown[]
+    hashNavigation?: unknown
   }
 }
